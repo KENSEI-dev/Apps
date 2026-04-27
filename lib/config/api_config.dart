7b26fe6043f8e016254e4c03e156374ad07f8dd1
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2026-04-28 03:06:35 +0530

    Update API URL to Cloudflare tunnel

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.0.103:8000';
+  static const String baseUrl = 'https://yourself-conditions-shareware-rear.trycloudflare.com';
   static const String signupEndpoint = '/auth/signup';
   static const String loginEndpoint = '/auth/login';
   static const String meEndpoint = '/auth/me';
