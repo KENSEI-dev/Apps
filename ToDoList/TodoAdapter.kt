9344f1181ac0106fd56e8043d9fa775b2c829d3c
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2026-06-09 18:30:04 +0530

    Refactor TodoAdapter with new features
    Updated TodoAdapter to support item click and check change callbacks.

--- ToDoList/TodoAdapter.kt
+++ ToDoList/TodoAdapter.kt
@@ -1,17 +1,18 @@
 package com.example.todolist
+
 import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
-
 import com.example.todolist.databinding.ItemTodoBinding
 
 class TodoAdapter(
-    private val todos: MutableList<Todo>
+    private var todos: List<Todo>,
+    private val onCheckChanged: (Todo, Boolean) -> Unit = { _, _ -> },
+    private val onItemClick: (Todo) -> Unit = {}
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
     class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
@@ -23,21 +24,19 @@ class TodoAdapter(
             )
         )
     }
-    fun addTodo(todo: Todo) {
-        todos.add(todo)
-        notifyItemInserted(todos.size - 1)
-    }
-    fun deleteDoneTodos() {
-        todos.removeAll { todo ->
-            todo.isChecked
-        }
+
+    fun updateTodos(newTodos: List<Todo>) {
+        todos = newTodos
         notifyDataSetChanged()
     }
+
     private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
-        if(isChecked) {
-            tvTodoTitle.paintFlags=tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
-            } else {
-            tvTodoTitle.paintFlags=tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        if (isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+            tvTodoTitle.alpha = 0.5f // Visual change: fade out completed tasks
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+            tvTodoTitle.alpha = 1.0f
         }
     }
 
@@ -45,16 +44,21 @@ class TodoAdapter(
         val curTodo = todos[position]
         holder.binding.apply {
             tvTodoTitle.text = curTodo.title
+            
+            cbDone.setOnCheckedChangeListener(null)
             cbDone.isChecked = curTodo.isChecked
             toggleStrikeThrough(tvTodoTitle, curTodo.isChecked)
+            
             cbDone.setOnCheckedChangeListener { _, isChecked ->
-                toggleStrikeThrough(tvTodoTitle, isChecked)
-                curTodo.isChecked = !curTodo.isChecked
+                onCheckChanged(curTodo, isChecked)
+            }
+
+            // Implement navigation to detail page on item click
+            root.setOnClickListener {
+                onItemClick(curTodo)
             }
         }
     }
-
-
 
     override fun getItemCount(): Int {
         return todos.size
