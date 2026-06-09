b9f7cc0b931412c45ed88f6bdffc92e84f3f626d
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2026-06-09 18:23:12 +0530

    Implement navigation drawer and fragment replacement
    Refactor MainActivity to implement a navigation drawer and replace fragments for task management.

--- ToDoList/MainActivity.kt
+++ ToDoList/MainActivity.kt
@@ -1,43 +1,58 @@
 package com.example.todolist
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
 import com.example.todolist.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
-    private var todoAdapter=TodoAdapter(mutableListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Setup Edge-to-Edge and Window Insets
+        ViewCompat.setOnApplyWindowInsetsListener(binding.drawerLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        
-        todoAdapter = TodoAdapter(mutableListOf())
-        binding.rvTodoItems.adapter=todoAdapter
-        binding.rvTodoItems.layoutManager= LinearLayoutManager(this)
 
-        binding.btnAddTodo.setOnClickListener {
-            val todoTitle= binding.etTodoTitle.text.toString()
-            if(todoTitle.isNotEmpty()){
-                val todo=Todo(todoTitle)
-                todoAdapter.addTodo(todo)
-                binding.etTodoTitle.text.clear()
+        // Navigation Drawer Item Selection
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_tasks -> replaceFragment(TodoFragment())
+                R.id.nav_deleted -> replaceFragment(DeletedFragment())
             }
+            true
         }
-        binding.btnDeleteDoneTodos.setOnClickListener {
-            todoAdapter.deleteDoneTodos()
-        }
+
+        // Load the default fragment on start
+        if (savedInstanceState == null) {
+            replaceFragment(TodoFragment())
+            binding.navView.setCheckedItem(R.id.nav_tasks)
+            
+            // Visible change: Show a welcome message
+            Snackbar.make(binding.root, "Welcome to Todo Master!", Snackbar.LENGTH_LONG).show()
         }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+    }
+}
