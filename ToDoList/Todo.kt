cf6f78f5eef4b2b3513655a6c194fd122d4adc32
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2026-06-09 18:29:30 +0530

    Add isDeleted property to Todo data class

--- ToDoList/Todo.kt
+++ ToDoList/Todo.kt
@@ -1,6 +1,6 @@
 package com.example.todolist
 
 data class Todo (
-    val title: String,
-    var isChecked: Boolean=false
+    val title: String, var isChecked: Boolean=false,
+    var isDeleted: Boolean=false
 )
